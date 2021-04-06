3861c38b40bacd5b040334188d638b7cc33c09b2
ahunter135 <ahunter8850@outlook.com>
2021-04-06 14:36:29 -0500

    no message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  changeMyName(email, password) {
+    /*
+    Do login logic here, then move on
+    */
+    //Check is email is email
+
+    //Validate password is 6 or more characters
+
+    // POST DATA HERE 
+
+    // Got a success, move on now
+    this.router.navigateByUrl("welcome");
+  }
 }

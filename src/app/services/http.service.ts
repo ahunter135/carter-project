fbf26b122fa27c095b98e0e500535fdd71aebba8
ahunter135 <ahunter8850@outlook.com>
2021-04-07 14:11:33 -0500

    Add http service

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  base = "put base endpoint here";
+  constructor(private http: HttpClient) { }
+
+  /**
+   * This endpoint takes some data and an endpoint you want to send the data to.
+   * 
+   * Keep it private so we don't accidentally use it somewhere else
+   * 
+   * @param endpoint 
+   * @param data 
+   * @returns HTTP Response
+   */
+  private async post(endpoint, data) {
+    let url = this.base + endpoint;
+    let headers = {};
+    let config = <any>{
+      url: url,
+      method: "POST",
+      headers: headers,
+      observe: 'response'
+    }
+    let response = await <any>this.http.post(url, data, config).toPromise();
+    return response;
+  }
+
+  /**
+   * This is a publically accessible function to POST login data to the login endpoint
+   * 
+   * @param data 
+   */
+  async login(data) {
+    let responseData = await this.post("login", data);
+    return responseData;
+  }
+}
